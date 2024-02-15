30f341ceba298e67a527d6f9ceee44e6a4f6a925
hasevraqyl <119125655+hasevraqyl@users.noreply.github.com>
2024-02-15 19:53:49 +0300

    Add files via upload

---
+++ bot.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/bwmarrin/discordgo"
+)
+
+type Init struct {
+	Token   string
+	GuildID string
+}
+
+var info Init
+
+func init() {
+	f := "setup_secret.toml"
+	if _, err := os.Stat(f); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := toml.DecodeFile(f, &info); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var dg *discordgo.Session
+var (
+	integerOptionMinValue          = 0.0
+	dmPermission                   = false
+	defaultMemberPermissions int64 = discordgo.PermissionManageServer
+	commands                       = []*discordgo.ApplicationCommand{
+		{
+			Name:        "test",
+			Description: "test command",
+		},
+		{
+			Name:        "options",
+			Description: "Command for demonstrating options",
+			Options: []*discordgo.ApplicationCommandOption{
+
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "string-option",
+					Description: "String option",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Name:        "integer-option",
+					Description: "Integer option",
+					MinValue:    &integerOptionMinValue,
+					MaxValue:    10,
+					Required:    true,
+				},
+			},
+		},
+	}
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"test": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "placeholder",
+				},
+			})
+		},
+		"options": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			options := i.ApplicationCommandData().Options
+			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+			for _, opt := range options {
+				optionMap[opt.Name] = opt
+			}
+			var b strings.Builder
+			b.WriteString("placeholder:\n")
+
+			if option, ok := optionMap["string-option"]; ok {
+				b.WriteString(option.StringValue())
+			}
+
+			if opt, ok := optionMap["integer-option"]; ok {
+				b.WriteString(fmt.Sprint(opt.IntValue()))
+			}
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: b.String(),
+				},
+			})
+		},
+	}
+)
+
+func init() {
+	var err error
+	dg, err = discordgo.New("Bot " + info.Token)
+	if err != nil {
+		log.Printf("error creating session %v", err)
+		return
+	}
+}
+
+func init() {
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func main() {
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err := dg.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dg.Close()
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, info.GuildID, v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		registeredCommands[i] = cmd
+	}
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	fmt.Println("removing commands...")
+	for _, v := range registeredCommands {
+		err := dg.ApplicationCommandDelete(dg.State.User.ID, info.GuildID, v.ID)
+		if err != nil {
+			log.Panicf("cannot delete '%v' command: %v", v.Name, err)
+		}
+	}
+	fmt.Println("shutdown.")
+}
